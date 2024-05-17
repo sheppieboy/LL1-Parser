188b09be6686738471be11c12295e6f8c812e5d9
Luke Sheppard <sheppiegit@gmail.com>
2024-05-16 22:53:16 -0400

    Added the lexer and lexer tests for the monkey lang.

---
+++ lexer_test.go
@@ -0,0 +1,120 @@
+package lexer
+
+import (
+	"interpreter-monkey/token"
+	"testing"
+)
+
+func TestNextToken(t *testing.T){
+	input := `let five = 5;
+	
+	let ten = 10;
+	let add = fn(x, y){
+		x + y;
+	};
+	let result = add(five, ten);
+	!-/*5;
+    5 < 10 > 5;
+	if else true false return;
+	10 == 9;
+	10 != 9;
+	"foobar"
+	"foo bar"
+	[1,2]
+	{"foo": "bar"}
+	`
+
+	tests := []struct {
+		expectedType token.TokenType
+		expectedLiteral string
+	}{	
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.ELSE, "else"},
+		{token.TRUE, "true"},
+		{token.FALSE, "false"},
+		{token.RETURN, "return"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
+		{token.STRING, "foobar"},
+		{token.STRING, "foo bar"},
+		{token.LBRACKET, "["},
+		{token.INT, "1"},
+		{token.COMMA, ","},
+		{token.INT, "2"},
+		{token.RBRACKET, "]"},
+		{token.LBRACE, "{"},
+		{token.STRING, "foo"},
+		{token.COLON, ":"},
+		{token.STRING, "bar"},
+		{token.RBRACE, "}"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+	for i, tt := range tests{
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType{
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral{
+			t.Fatalf("tests[%d] - token literal wrong. expected=%q, go=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
