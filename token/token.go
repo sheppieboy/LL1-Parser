7ce25b289aa245e0c9591af1b1eae3230f6bfc6a
Luke Sheppard <sheppiegit@gmail.com>
2024-05-16 22:49:13 -0400

    tokens for monkey language.

---
+++ token/token.go
@@ -0,0 +1,70 @@
+package token
+
+type TokenType string
+
+type Token struct{
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	//identifiers + literals
+
+	IDENT = "IDENT"
+	INT = "INT"
+	STRING = "STRING"
+
+	//Operators
+	ASSIGN = "="
+	PLUS = "+"
+	MINUS = "-"
+	BANG = "!"
+	ASTERISK = "*"
+	SLASH = "/"
+
+	LT = "<"
+	GT = ">"
+	EQ = "=="
+	NOT_EQ = "!="
+
+	//Delimiters
+	COMMA = ","
+	SEMICOLON = ";"
+	COLON = ":"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+	LBRACKET = "["
+	RBRACKET = "]"
+
+	//keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+	IF = "IF"
+	ELSE = "ELSE"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	RETURN = "RETURN"
+)
+
+var keywords = map[string]TokenType{
+	"fn": FUNCTION,
+	"let": LET,
+	"if": IF,
+	"else": ELSE,
+	"true": TRUE,
+	"false": FALSE,
+	"return": RETURN,
+}
+
+func LookupIdentifier(ident string) TokenType{
+	if tok, ok := keywords[ident]; ok{
+		return tok
+	}
+	return IDENT
+}
